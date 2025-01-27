4dd0d4f9f371df8857d9565a0c315093904b94b6
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2025-01-27 09:43:01 -0300

    att

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,7 +3,7 @@ import styles from './Inicio.module.css'
 function Inicio(){
     return (
         <section className={styles.inicio}>
-            
+            <h1>São Paulo</h1>
         </section>
     )
 }
