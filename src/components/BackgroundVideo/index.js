a2493071f8d7e616a448c09bdba8cabc30480d3f
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2025-02-11 10:13:32 -0300

    tela de inicio

--- src/components/BackgroundVideo/index.js
+++ src/components/BackgroundVideo/index.js
@@ -1,5 +1,6 @@
-import styles from "./BackgroundVideo.modulo.css";
+import styles from "./BackgroundVideo.module.css";
 import videoBg from "./gif-spfc.mp4";
+
 
 const BackgroundVideo = () => {
     return (
@@ -8,9 +9,11 @@ const BackgroundVideo = () => {
                 <source src={videoBg} type="video/mp4" />
                 Seu navegador não suporta vídeos HTML5.
             </video>
-            <div className={styles.content}>
-                <h1>Bem-vindo</h1>
-                <p>Esse é um fundo de vídeo</p>
+            <div className={styles.titulo}>
+                <img src='/images/logotipo-spfc.png' alt='Logo'></img>
+                <h1>São Paulo</h1>
+                <h2>Futebol Clube</h2>
+                
             </div>
         </div>
     )
