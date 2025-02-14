1ea7f187d9a6e337bf92e2064951c88cbff8bb34
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2025-02-14 09:41:58 -0300

    pagina inicial concluida + começo da pagina sobre

---
+++ src/pages/Sobre/index.js
@@ -0,0 +1,9 @@
+import Header from "components/Header";
+
+function Sobre() {
+    return (
+        <Header/>  
+    )
+}
+
+export default Sobre;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./pages/Inicio";
+import Sobre from "./pages/Sobre";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />}/>
+        <Route path="/Sobre" element={<Sobre />}/>
 
         <Route path="*" element={<div>ERROR 404</div>}/>
       </Routes>
