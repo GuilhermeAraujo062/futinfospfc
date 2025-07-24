c1f3af3778611b3caa44a95c1f02e4c969ade6a1
Guilherme Araujo <contatoguilherme3110@gmail.com>
2025-07-24 12:28:58 -0300

    Reservas e componentes
    Adicionado os jogadores reservas e código movido para componentes

---
+++ src/components/JogadoresReserva/index.js
@@ -0,0 +1,107 @@
+import JogadoresTitulares from "components/JogadoresTitulares";
+import styles from "./JogadoresReservas.module.css";
+
+function JogadoresReservas() {
+    return(
+        <div className={styles.reservas}>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/e0/b5/18/e0b518e87363f8891fd43e5eae6c4323.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Jandrey</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/45/51/76/45517665164813de6e74bb2b92fb7912.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Maik</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/4b/e2/da/4be2da0cfa9791f10a7e01759c6bb710.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Wendell</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/37/bb/31/37bb314aad9f0827b57759fe91ee045b.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>P.Lanza</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/e3/6b/19/e36b199afa709c32dff6f1ccebd69fce.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Ferraresi</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/cf/bf/8c/cfbf8c507b7bc2ceda57fc7b3d39b0d3.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Sabino</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/6d/48/5d/6d485d16f575a2895c4d0180fa5026ca.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>D.Bobadilla</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/13/22/7e/13227ee83b87b360c7661579b9ee5bd2.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>M.Antônio</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/73/20/43/732043c17945aa9936fedac2b31e0b6d.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>L.Gustavo</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/ad/31/dd/ad31dd4ba7762143ee7dcbf3a6f14cbd.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Luan</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/ea/28/40/ea284008516dfe7308df204457310e48.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Negrucci</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/67/94/d4/6794d4b9eb419a3f61ee5855377fe65a.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Rodriguinho</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/3f/3e/97/3f3e979a76aaf8477a13c196880c323e.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Ferreirinha</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/21/1c/f1/211cf14c4a815bd5dbbee64cd200d92b.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>G.Tapia</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/de/26/15/de2615b4cbc4f5914a33b04f51fc4ce0.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>A.Silva</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/0b/6b/90/0b6b90961b3000681a9471bb74a5f9ab.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>J.Dinenno</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/46/18/e1/4618e1f5f2f2663e816785a7dbcda80d.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>Lucca</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/b1/08/e7/b108e7fa34c6fc1d6080ada9cbb6e066.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>L.Ferreira</h3>
+                    </div>
+
+                    <div className={styles.jogador}>
+                        <img src="https://i.pinimg.com/736x/ad/fb/cb/adfbcb9e11cf0d328b7242502d5dbfdb.jpg" alt="foto do jogador" className={styles.img_jogador}></img>
+                        <h3 className={styles.nome}>R.Francisco</h3>
+                    </div>
+
+                </div>
+    )
+}
+
+export default JogadoresReservas;
